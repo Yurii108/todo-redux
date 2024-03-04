391d333095a42fffa4e070c27aad1e7b5d73d7da
Yurii Paraska <paraska108@gmail.com>
2024-03-04 22:06:30 +0200

    f commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { LayoutPage } from "./layout";
+
+function App() {
+  return (
+    <>
+      <LayoutPage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/layout/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Layout, Flex } from "antd";
+
+const { Header, Footer, Content } = Layout;
+
+export const LayoutPage: React.FC = () => (
+  <Flex gap="middle" wrap="wrap">
+    <Layout style={layoutStyle}>
+      <Header style={headerStyle}>Header</Header>
+      <Content style={contentStyle}>Content</Content>
+      <Footer style={footerStyle}>Footer</Footer>
+    </Layout>
+  </Flex>
+);
+
+const headerStyle: React.CSSProperties = {
+  textAlign: "center",
+  color: "#fff",
+  height: 64,
+  paddingInline: 48,
+  lineHeight: "64px",
+  backgroundColor: "#4096ff",
+};
+
+const contentStyle: React.CSSProperties = {
+  textAlign: "center",
+  minHeight: 120,
+  lineHeight: "120px",
+  color: "#fff",
+  backgroundColor: "#0958d9",
+};
+
+const footerStyle: React.CSSProperties = {
+  textAlign: "center",
+  color: "#fff",
+  backgroundColor: "#4096ff",
+};
+
+const layoutStyle = {
+  overflow: "hidden",
+  width: "100%",
+  height: "100vh",
+};
