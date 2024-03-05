8e4087fd61d644e72fd2b5aff0f02d6f82fe0985
Yurii Paraska <paraska108@gmail.com>
2024-03-05 22:49:08 +0200

    created store redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { Provider } from "react-redux";
+
 import "./App.css";
+import store from "./redux/store";
+
 import { LayoutPage } from "./layout";
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <LayoutPage />
-    </>
+    </Provider>
   );
 }
 
---
+++ src/redux/store.tsx
@@ -0,0 +1,8 @@
+import { legacy_createStore as createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import todoReducer from "./reducers";
+
+const store = createStore(todoReducer, composeWithDevTools());
+
+export default store;
